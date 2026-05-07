0b2545b6a7f7aa751b29ef942a55f2c959250f14
Dustin Carmelo <dustcarmelo@yahoo.com>
2026-05-06 21:32:48 -0400

    added assetManaget, added assets

--- src/gameLayer/gameMain.cpp
+++ src/gameLayer/gameMain.cpp
@@ -6,12 +6,13 @@
 // sure this is a global struct, but it's only global in this cpp file
 struct GameData
 {
-	float positionX = 100;
-	float positionY = 100;
+	Texture dirtTexture;
 }gameData;
 
 bool initGame()
 {
+	gameData.dirtTexture = LoadTexture(RESOURCES_PATH "dirt.png");
+
 	return true;
 }
 
@@ -21,12 +22,8 @@ bool updateGame()
 	// cap deltaTime at 5 frames per second
 	if (deltaTime > 1.f / 5.f) { deltaTime = 1.f / 5.f; }
 
-	if (IsKeyDown(KEY_A)) { gameData.positionX -= 200.f * deltaTime; }
-	if (IsKeyDown(KEY_D)) { gameData.positionX += 200.f * deltaTime; }
-	if (IsKeyDown(KEY_W)) { gameData.positionY -= 200.f * deltaTime; }
-	if (IsKeyDown(KEY_S)) { gameData.positionY += 200.f * deltaTime; }
-
-	DrawRectangle(gameData.positionX, gameData.positionY, 50, 50, BLUE);
+	DrawTexturePro(gameData.dirtTexture, { 0,0,(float)gameData.dirtTexture.width,(float)gameData.dirtTexture.height },
+		{ 100,50,100,100 }, {}, 0, WHITE);
 
 	return true;
 }
