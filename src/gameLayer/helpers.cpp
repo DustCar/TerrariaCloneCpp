ae9bf5aa7ea7c8abe09330d6cd274e32ba387bf6
DustCar <dustcarmelo@yahoo.com>
2026-08-22 09:31:42 -0400

    added vector version of readEntireFile

--- src/gameLayer/helpers.cpp
+++ src/gameLayer/helpers.cpp
@@ -30,6 +30,25 @@ bool readEntireFile(const char* fileName, std::string& data)
 	return true;
 }
 
+bool readEntireFile(const char* fileName, std::vector<char>& data)
+{
+	std::ifstream f(fileName, std::ios::binary);
+
+	if (!f.is_open()) { return false; }
+
+	f.seekg(0, std::ios::end);
+	size_t size = f.tellg();
+	if (size < 0) { return false; }
+
+	f.seekg(0, std::ios::beg);
+
+	data.resize(size);
+
+	f.read(data.data(), size);
+
+	return true;
+}
+
 bool writeEntireFile(const char* fileName, const void* data, size_t size)
 {
 	std::ofstream f(fileName, std::ios::binary);
--- src/gameLayer/helpers.h
+++ src/gameLayer/helpers.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <raylib.h>
+#include <string>
+#include <vector>
 
 // obtain a certain texture from an atlas
 Rectangle getTextureAtlas(int x, int y, int cellSizePixelsX, int cellSizePixelsY);
@@ -9,6 +11,8 @@ float lerp(float a, float b, float t);
 
 // implement read entire file func
 bool readEntireFile(const char* fileName, std::string& data);
+
+bool readEntireFile(const char* fileName, std::vector<char>& data);
 
 // implement write entire file func
 bool writeEntireFile(const char* fileName, const void* data, size_t size);
